c00a329293d1e56730d8e4dbb84f24406f7114db
zhaihaoran <359118915@qq.com>
2017-10-26 19:10:16 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,7 @@ const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: {
-        index: __dirname + '/src/js/index'
-    },
+    entry: { index: __dirname + '/src/sokudo/js/index' },
     output: {
         path: __dirname + "/docs",
         filename: '[name].js'
@@ -44,7 +42,7 @@ module.exports = {
         new webpack.BannerPlugin('版权所有，翻版必究'),// 在打包后的代码中添加注释
         // 简历
         new HtmlWebpackPlugin({
-            template: __dirname + '/src/index.html' // new 一个这个插件的实例，并传入相关的参数
+            template: __dirname + '/src/sokudo/index.html' // new 一个这个插件的实例，并传入相关的参数
         }),
         new webpack.optimize.UglifyJsPlugin(), //压缩js代码
     ]
